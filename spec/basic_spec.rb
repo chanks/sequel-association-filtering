d2e1e722181ebd6549391d499d7e583f4525e8f1
Chris Hanks <christopher.m.hanks@gmail.com>
2018-06-01 13:30:05 -0400

    Start getting ready to expand basic specs.

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -4,20 +4,39 @@ require 'spec_helper'
 
 class BasicSpec < AssociationFilteringSpecs
   before do
+    drop_tables
+
     DB.create_table :artists do
       primary_key :id
     end
 
+    DB.create_table :albums do
+      primary_key :id
+      foreign_key :artist_id, :artists
+    end
+
     DB.run <<-SQL
       INSERT INTO artists SELECT i FROM generate_series(1, 10) i;
+      INSERT INTO albums (artist_id) SELECT (i % 10) + 1 FROM generate_series(1, 100) i;
     SQL
 
     class Artist < Sequel::Model
+      one_to_many :albums, class: 'BasicSpec::Album'
+    end
+
+    class Album < Sequel::Model
+      many_to_one :artist, class: 'BasicSpec::Artist'
     end
   end
 
   after do
-    DB.drop_table? :artists
+    BasicSpec.send(:remove_const, :Artist)
+    BasicSpec.send(:remove_const, :Album)
+    drop_tables
+  end
+
+  def drop_tables
+    DB.drop_table? :albums, :artists
   end
 
   describe "association_filter" do
@@ -38,5 +57,7 @@ class BasicSpec < AssociationFilteringSpecs
 
       assert_equal "cannot pass more than one of :at_least, :at_most, and :exactly", error.message
     end
+
+    it "should be able to return a cached dataset"
   end
 end
